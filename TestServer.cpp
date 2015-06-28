00236490b33ebad634bf617462f2016a8e27dd68
Ricardo Delfin <ricardo.delfin@utexas.edu>
2015-06-27 19:59:31 -0700

    Added Visual Studio code

---
+++ TestServer.cpp
@@ -0,0 +1,13 @@
+// TestServer.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello world!" << endl;
+	return 0;
+}
+
